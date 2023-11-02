85bf02796876579c5b8141c91e35e2b132f66279
miqbalramadhan18 <miqbalr18@gmail.com>
2023-11-03 03:05:55 +0700

    update

---
+++ geojson_test.go
@@ -0,0 +1,60 @@
+package gcfbackendbal
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	pasproj "github.com/HRMonitorr/PasetoprojectBackend"
+	"github.com/whatsauth/watoken"
+)
+
+//func TestGCHandlerFunc(t *testing.T) {
+//	data := GCHandlerFunc("string", "GIS", "geogis")
+//
+//	fmt.Printf("%+v", data)
+//}
+
+func TestGeneratePaseto(t *testing.T) {
+	//privateKey, publicKey := watoken.GenerateKey()
+	//fmt.Println(privateKey)
+	//fmt.Println(publicKey)
+	hasil, err := watoken.Encode("iqbal", "PrivateKey")
+	fmt.Println(hasil, err)
+}
+
+func TestUpdateData(t *testing.T) {
+	data := LonLatProperties{
+		Type:   "Polygon",
+		Name:   "qobel",
+		Volume: "1",
+	}
+	up := UpdateNameGeo("MONGOSTRING", "gisgcp", context.Background(), data)
+	fmt.Println(up)
+}
+
+func TestDeleteDataGeo(t *testing.T) {
+	data := LonLatProperties{
+		Type:   "Polygon",
+		Name:   "qobel",
+		Volume: "1",
+	}
+	up := DeleteDataGeo("MONGOSTRING", "gisgcp", context.Background(), data)
+	fmt.Println(up)
+}
+
+func TestInsertUser(t *testing.T) {
+	conn := GetConnectionMongo("MONGOSTRING", "gisgcp")
+	pass, _ := pasproj.HashPass("qobel123")
+	data := RegisterStruct{
+		Username: "iqbal",
+		Password: pass,
+	}
+	ins := InsertUserdata(conn, data.Username, data.Password)
+	fmt.Println(ins)
+}
+
+func TestIsexist(t *testing.T) {
+	data := IsExist("token", "PublicKey")
+	fmt.Println(data)
+}
